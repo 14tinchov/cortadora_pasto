c28afe1714c46ebf0a7b83502418c32640d3ab81
Martin Villalba <14tinchov@gmail.com>
2016-05-09 20:52:38 -0300

    otro estado

--- cortadora.py
+++ cortadora.py
@@ -1,28 +1,34 @@
 import pygame
+from cuadrante import Cuadrante
 from random import randint
 
 class Cortadora(object):
     
   def __init__(self, mapa):
-    self.mapa = mapa
-
-    self.velocidad = 2
-    self.tiene_posicion_inicial = False
     self.busca_contorno = False
-    self.nuevo_destino = False
+    self.tiene_posicion_inicial = False
+
+    self.mapa = mapa # esto tiene eterno de prueba
+    self.cuadrantes = []
+
+    self.motor = False # esto sirve para pintar los pastos tocados
+    self.velocidad = 2
+
+
+    # si esta en false quiere decir q la cortadora se esta moviendo por lo tanto no busca
+    # una nueva posicion
     self.buscar_nueva_pos = True
     
-    self.posicion_inicial_x = 0
-    self.posicion_inicial_y = 0
+    # sirve para armar nuestro propio sistemas de cordenadas
+    self.nueva_posicion_x = 0
+    self.nueva_posicion_y = 0
 
+    # sirve para armar tirar una posicion aleatoria en el entorno
     self.posicion_x = randint(1, 18)
     self.posicion_y = randint(1, 14)
 
-    
-
-    #fila , columna
+    #elige un pasto al azar dentro del entorno
     lugar_inicial = mapa[self.posicion_y][self.posicion_x]
-
     x = lugar_inicial.rect.left
     y = lugar_inicial.rect.top
 
@@ -32,22 +38,19 @@ class Cortadora(object):
     self.rect = pygame.Rect(x, y, 16, 16)
 
   def mover(self):
+
     if self.rect.x != self.posicion_destino_x:
-      # print "x:"
-      # print "actual: %f" % self.rect.x
-      # print "destino: %f" % self.posicion_destino_x
+
       if self.rect.x < self.posicion_destino_x:
-        self.rect.x += 1
+        self.rect.x += self.velocidad
       else:
-        self.rect.x -= 1
+        self.rect.x -= self.velocidad
+
     if self.rect.y != self.posicion_destino_y:
-      # print "y:"
-      # print "actual: %f" % self.rect.y
-      # print "destino: %f" % self.posicion_destino_y
       if self.rect.y < self.posicion_destino_y:
-        self.rect.y += 1
+        self.rect.y += self.velocidad
       else:
-        self.rect.y -= 1
+        self.rect.y -= self.velocidad
 
     if self.rect.x == self.posicion_destino_x and self.rect.y == self.posicion_destino_y:
       self.buscar_nueva_pos = True
@@ -58,16 +61,21 @@ class Cortadora(object):
   def detectar_posicion_inicial(self):
     if self.buscar_nueva_pos == True:
       self.buscar_nueva_pos = False
-      nueva_pos = self.moverse_derecha()
 
-      if self.detectar_obstaculo("derecha"):
+      if self.detectar_obstaculo("derecha") and self.detectar_obstaculo('abajo'):
         print "no moverse"
         self.tiene_posicion_inicial = True
         self.busca_contorno = True
-        self.posicion_inicial_x = nueva_pos.rect.left
-        self.posicion_inicial_y = nueva_pos.rect.top
+        self.motor = True
+        self.cargar_cuadrante(self.nueva_posicion_x, self.nueva_posicion_y)
+        self.imprimir_cuadrantes()
       else:
         print "moverse"
+        if self.detectar_obstaculo("derecha"):
+          nueva_pos = self.moverse_abajo()
+        else:
+          nueva_pos = self.moverse_derecha()
+
         self.posicion_destino_x = nueva_pos.rect.left
         self.posicion_destino_y = nueva_pos.rect.top
 
@@ -81,36 +89,44 @@ class Cortadora(object):
 
   def detectar_arriba(self):
     nueva_posicion = self.mapa[self.posicion_y - 1][self.posicion_x]
-    return self.hay_pasto(nueva_posicion)
+    return self.hay_obstaculo(nueva_posicion)
 
   def detectar_abajo(self):
     nueva_posicion = self.mapa[self.posicion_y + 1][self.posicion_x]
-    return self.hay_pasto(nueva_posicion)
+    return self.hay_obstaculo(nueva_posicion)
 
   def detectar_izquierda(self):
     nueva_posicion = self.mapa[self.posicion_y][self.posicion_x - 1]
-    return self.hay_pasto(nueva_posicion)
+    return self.hay_obstaculo(nueva_posicion)
 
   def detectar_derecha(self):
     nueva_posicion = self.mapa[self.posicion_y][self.posicion_x + 1]
-    return self.hay_pasto(nueva_posicion)
+    return self.hay_obstaculo(nueva_posicion)
 
-  def hay_pasto(self, posicion):
+  def hay_obstaculo(self, posicion):
     if posicion.__class__.__name__ == "Obstaculo":
       return True
     else:
       return False
 
   def moverse_arriba(self):
+    if self.tiene_posicion_inicial:
+      self.nueva_posicion_y -= 1
     self.posicion_y -= 1
     return self.mapa[self.posicion_y][self.posicion_x]
   def moverse_abajo(self):
+    if self.tiene_posicion_inicial:
+      self.nueva_posicion_y += 1
     self.posicion_y += 1
     return self.mapa[self.posicion_y][self.posicion_x]
   def moverse_izquierda(self):
+    if self.tiene_posicion_inicial:
+      self.nueva_posicion_x -= 1
     self.posicion_x -= 1
     return self.mapa[self.posicion_y][self.posicion_x]
   def moverse_derecha(self):
+    if self.tiene_posicion_inicial:
+      self.nueva_posicion_x += 1
     self.posicion_x += 1
     return self.mapa[self.posicion_y][self.posicion_x]
 
@@ -128,6 +144,8 @@ class Cortadora(object):
         print "izquierda"
         nueva_pos = self.moverse_izquierda()
         self.posicion_destino_x = nueva_pos.rect.left
+        self.cargar_cuadrante(self.nueva_posicion_x, self.nueva_posicion_y)
+        self.imprimir_cuadrantes()
         return
       if self.detectar_obstaculo('izquierda') == True  and self.detectar_obstaculo('arriba') == False:
         print "arriba"
@@ -140,5 +158,15 @@ class Cortadora(object):
         self.posicion_destino_x = nueva_pos.rect.left
         return
 
+  def cargar_cuadrante(self, pos_x, pos_y):
+    obs_arr = self.detectar_arriba()
+    obs_aba = self.detectar_abajo()
+    obs_izq = self.detectar_izquierda()
+    obs_der = self.detectar_derecha()
+    nuevo_cuandrante = Cuadrante(pos_x, pos_y, obs_arr, obs_aba, obs_izq, obs_der)
+    self.cuadrantes.append(nuevo_cuandrante)
 
-# abajo, izq, arriba, derecha
+  def imprimir_cuadrantes(self):
+    for i in range(0, len(self.cuadrantes)):
+      print "Cuadrante %d:" % i
+      print "Pos x: %d, Pos y: %d" %(self.cuadrantes[i].posicion_x, self.cuadrantes[i].posicion_y)
--- cortadora_pasto.py
+++ cortadora_pasto.py
@@ -55,11 +55,12 @@ while running:
   
   # Mover de forma aleatoria
   # cortadora.mover(obstaculos, pastos, pantalla)
+  if cortadora.busca_contorno == True:
+    cortadora.recorrer_contorno()
+
   if cortadora.tiene_posicion_inicial == False:
     cortadora.detectar_posicion_inicial()
 
-  if cortadora.busca_contorno == True:
-    cortadora.recorrer_contorno()
 
   cortadora.mover()
 
@@ -67,12 +68,18 @@ while running:
   pantalla.fill((0, 0, 0))
 
   for obs in obstaculos:
+    if cortadora.rect.colliderect(obs.rect) and cortadora.motor == True:
+      obs.tocado = True
+
     if obs.tocado == True:
       pygame.draw.rect(pantalla, (200, 100, 100), obs.rect)
     else:
       pygame.draw.rect(pantalla, (255, 255, 255), obs.rect)
 
   for pasto in pastos:
+    if pasto.rect.colliderect(cortadora.rect) and pasto.tocado == False  and cortadora.motor == True:
+      pasto.tocado = True
+
     if pasto.tocado == True:
       pygame.draw.rect(pantalla, (000, 255, 000), pasto.rect)
     else:
@@ -80,4 +87,3 @@ while running:
 
   pygame.draw.rect(pantalla, (0, 96, 96), cortadora.rect)
   pygame.display.flip()
-
